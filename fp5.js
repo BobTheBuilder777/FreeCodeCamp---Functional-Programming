7f30abafb7be7feb24efb4550c624ad9802ea555
Noel Lau <noellaukuankiat03@sd.taylors.edu.my>
2023-07-27 12:36:50 +0800

    Pass Arguments to Avoid External Dependence in a Function

---
+++ fp5.js
@@ -0,0 +1,11 @@
+// Pass Arguments to Avoid External Dependence in a Function
+
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(arg) {
+  return arg + 1;
+
+  // Only change code above this line
+}
